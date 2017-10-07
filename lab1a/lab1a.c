d2347885facbd138915be65d72ac3a3756da5657
Michael Donald Wu <cheeserules43@gmail.com>
2017-10-07 12:19:55 -0700

    added options

--- lab1a/lab1a.c
+++ lab1a/lab1a.c
@@ -4,6 +4,7 @@
 #include<unistd.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<sys/types.h>
 
 struct termios originalTerminalAttributes;
 void resetTerminal()
@@ -13,6 +14,61 @@ void resetTerminal()
 
 int main(int argc, char *argv[])
 {
+  char opt;
+  static struct option long_options[] =
+  {
+    {"shell", no_argument, 0, 's'},
+    {0, 0, 0, 0}
+  };
+
+  int pipefd[2];
+  int pipe2fd[2];
+  pid_t childpid=0;
+  while((opt=getopt_long(argc, argv, "", long_options, 0)) != -1)
+  {
+    switch(c){
+      case 's':
+	if(pipe(pipefd)==-1)
+	{
+	  fprintf(stderr, "Error making pipe 1: %s", strerror(errno));
+	  exit(1);
+        }
+	if(pipe(pipe2fd)==-1)
+	{
+	  fprintf(stderr, "Error making pipe 2: %s", strerror(errno));
+	  exit(1);
+        }
+	pid_t pid=fork();
+	if(pid==-1)
+	{
+	  fprintf(stderr, "Error forking: %s", strerror(errno));
+	  exit(1);
+	}
+	else if(pid==0)
+	{
+	  close(STDIN_FILENO);
+	  dup(pipefd[0]);
+	  close(pipefd[0]);
+	  close(pipefd[1]);
+
+	  close(STDOUT_FILENO);
+	  dup(pipe2fd[1]);
+	  close(pipe2fd[0]);
+	  close(pipe2fd[1]);
+	}
+	else
+        {
+	  close(pipefd[0]);
+	  close(pipe2fd[1]);
+	  childpid=pid;
+        }
+	break;
+      default:
+	fprintf(stderr, "Usage: %s [--shell]\n", argv[0]);
+	exit(1);
+    }
+  }
+  
   if(tcgetattr(STDIN_FILENO, &originalTerminalAttributes)<0)
   {
     fprintf(stderr, "Error getting terminal attributes: %s", strerror(errno));
@@ -30,7 +86,7 @@ int main(int argc, char *argv[])
   {
     fprintf(stderr, "Error setting terminal attributes: %s", strerror(errno));
     exit(1);
-  }
+  }  
 
   char buf[10];
   int numRead;
