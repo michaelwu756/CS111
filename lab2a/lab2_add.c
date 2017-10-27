41c710ef57576f8229a96bdc0f6de8881538959b
Michael Wu <cheeserules43@gmail.com>
2017-10-27 15:21:52 -0700

    added sync options and makefile to generate tests and graphs

--- lab2a/lab2_add.c
+++ lab2a/lab2_add.c
@@ -9,11 +9,34 @@
 int iterations;
 int opt_yield=0;
 char testType='\0';
+pthread_mutex_t mutex;
+int syncLock=0;
 void add(long long *pointer, long long value) {
-  long long sum = *pointer + value;
+  long long sum;
+  if(testType=='c')
+  {
+    do{
+      sum = *pointer + value;
+      if (opt_yield)
+        sched_yield();
+    }while(__sync_val_compare_and_swap(pointer, sum-value, sum)!=sum-value);
+    return;
+  }
+  if(testType=='m')
+    pthread_mutex_lock(&mutex);
+  if(testType=='s')
+    while(__sync_lock_test_and_set(&syncLock, 1)!=0)
+      ;
+
+  sum = *pointer + value;
   if (opt_yield)
     sched_yield();
   *pointer = sum;
+
+  if(testType=='m')
+    pthread_mutex_unlock(&mutex);
+  if(testType=='s')
+    __sync_lock_release(&syncLock);
 }
 
 void checkForError(int result, char *message)
@@ -51,8 +74,8 @@ void printCSV(int yield, char type, int threads, int iterations, struct timespec
   if(type=='\0')
     strcpy(t,"none");
   else
-    sprintf(t, "%c\0", type);
-  printf("add-%s%s,%d,%d,%d,%d,%d,%d\n",y,t,threads,iterations,operations,elapsedTimeNsec,elapsedTimeNsec/operations,counter);
+    sprintf(t, "%c", type);
+  printf("add-%s%s,%d,%d,%lld,%lld,%lld,%d\n",y,t,threads,iterations,operations,elapsedTimeNsec,elapsedTimeNsec/operations,counter);
 }
 
 int main(int argc, char  *argv[])
@@ -65,7 +88,7 @@ int main(int argc, char  *argv[])
     {"yield", no_argument, 0, 'y'},
     {0, 0, 0, 0}
   };
-  
+
   char c;
   int threads=0;
   iterations=0;
@@ -93,6 +116,8 @@ int main(int argc, char  *argv[])
   if(threads==0 || iterations==0 || (testType!='\0' && testType!='m' && testType!='s' && testType!='c'))
     printUsage(argv[0]);
 
+  pthread_mutex_init(&mutex, NULL);
+
   long long counter=0;
 
   struct timespec startTime;
@@ -104,9 +129,11 @@ int main(int argc, char  *argv[])
     checkForError(pthread_create(&thread[i],NULL,threadMain,&counter),"making thread");
   for(i=0; i<threads; i++)
     pthread_join(thread[i],NULL);
-  
+
   struct timespec endTime;
   checkForError(clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &endTime), "getting end time");
 
   printCSV(opt_yield, testType, threads, iterations, &startTime, &endTime, counter);
+
+  pthread_mutex_destroy(&mutex);
 }
